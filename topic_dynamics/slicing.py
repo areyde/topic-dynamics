7343f9955eb02d12340fb3a3e1987d55a5b54a56
areyde <areyde.elvgren@gmail.com>
2020-04-22 20:27:37 +0300

    Specified the time and the timezone of the checkout

--- topic_dynamics/slicing.py
+++ topic_dynamics/slicing.py
@@ -66,7 +66,7 @@ def checkout_by_date(repository: str, directory: str, before_date: datetime.date
     branch = cmdline("cd {repository}; git rev-parse --abbrev-ref HEAD"
                      .format(repository=repository))
     os.system("cp -r {repository} {directory}".format(repository=repository, directory=directory))
-    os.system('cd {directory}; '
-              'git checkout --quiet `git rev-list -n 1 --before="{date}" {branch}` > /dev/null'
+    os.system('cd {directory}; git checkout --quiet '
+              '`git rev-list -n 1 --before="{date} 00:00:00 0000" {branch}` > /dev/null'
               .format(directory=directory, date=before_date.strftime("%Y-%m-%d"), branch=branch))
     return cmdline("cd {directory}; git rev-parse HEAD".format(directory=directory))
