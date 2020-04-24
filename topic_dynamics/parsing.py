44a41b8789026abc5665464a7f71ed5883474a2f
areyde <areyde.elvgren@gmail.com>
2020-04-24 13:14:29 +0300

    Added the FileNotFound Exception

--- topic_dynamics/parsing.py
+++ topic_dynamics/parsing.py
@@ -255,7 +255,7 @@ def get_tokens_from_list(files_list: List[Tuple[str, str]]) -> Dict[str, List[Tu
     for file in files_list:
         try:
             file2tokens[file[0]] = get_tokens(file[0], file[1])
-        except UnicodeDecodeError:
+        except (UnicodeDecodeError, FileNotFoundError):
             continue
     return file2tokens
 
